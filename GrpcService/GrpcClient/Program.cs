6b23e375761e3af6650e776651c13b1ba5cb0139
Iren SALTALI <iren@saltali.com>
2021-05-03 16:44:47 +0300

    Local version

---
+++ GrpcService/GrpcClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Net.Client;
+using GrpcService;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpHandler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback =
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+            };
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions { HttpHandler = httpHandler });
+            var client = new Greeter.GreeterClient(channel);
+            HelloReply response = new HelloReply();
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                response = await client.SayHelloAsync(
+                      new HelloRequest
+                      {
+                          Name = ".NET 5 - grpcClient " + i
+                      });
+            }
+            Console.WriteLine("From Server: " + response.Message);
+            Console.ReadKey();
+        }
+    }
+}
